99097ad5d2b3948e6f737e6fd857e48198f7e7f2
Karim Ahmed <devKarimAhmed@gmail.com>
2024-09-27 02:51:25 +0300

    first commit

---
+++ lib/oop/oop.dart
@@ -0,0 +1,10 @@
+// Object Oriented Programming
+
+// 1- what is Object Oriented Programming
+// 2- Example
+// 3- advantages and disadvantages
+// 4- languages
+// 5- project (Same Projec in PP)
+void main(){
+
+}
---
+++ lib/pp/pp.dart
@@ -0,0 +1,11 @@
+// Procedural (Functional , Structural) Programming
+
+// 1- what is procedural programming
+// 2- Example
+// 3- advantages and disadvantages
+// 4- languages
+// 5- project (Same Projec in OOP)
+
+void main() {
+
+}
